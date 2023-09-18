19ca403ece20cb5ac1d31b74d1986b6e7a5b4f77
zainahammouri <130653709+zainahammouri@users.noreply.github.com>
2023-09-18 12:32:01 +0300

    ContactUs update

---
+++ JS Folder/ContactUs.js
@@ -0,0 +1,11 @@
+// function for sweet alert:
+function Alert(){
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Your Answer has been saved',
+        showConfirmButton: false,
+        timer: 3000
+      })
+}
+Alert();
